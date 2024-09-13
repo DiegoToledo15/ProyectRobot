11cf022a1bdcfb7bb5eb2841ef3d9083114b25cd
DiegoToledo <diegoantoniotoledo@gmail.com>
2024-09-12 21:30:50 -0300

    Actualización: arreglo codigo

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        // Crear una instancia del robot
         Robot robot = new Robot();
 
-        // Hacer que el robot camine
         robot.caminar();
 
-        // Aplicar un outfit casual al robot
         CasualOutfit casualOutfit = new CasualOutfit();
         robot.usarOutfit(casualOutfit);
 
-        // Aplicar un outfit formal al robot
         FormalOutfit formalOutfit = new FormalOutfit();
         robot.usarOutfit(formalOutfit);
     }
